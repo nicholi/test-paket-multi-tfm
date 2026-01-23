41cfa9291fa8a2c7bceadd928303f254ad48398f
Nicholas Schell <nicholas.schell@dolby.com>
2026-01-22 22:33:05 -0800

    initial checkin paket 10.0.1 net8 and net6 multi-tfm project

---
+++ TestLib/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestLib;
+
+public class Class1
+{
+    public static async Task TestFunction()
+    {
+        var list = new List<string>()
+        {
+            "one",
+            "2",
+        };
+
+        await foreach (var item in list.ToAsyncEnumerable())
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
